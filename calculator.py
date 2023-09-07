e1077955e61a58afe97f25e9a699a9dcd949b406
shashank9906 <123225695+shashank9906@users.noreply.github.com>
2023-09-07 14:29:25 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+calculation = ""
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
+def evaluate_calculation():
+    global calculation
+    print(calculation)
+    try:
+        result = str(eval(calculation))
+        calculation = ""
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, result)
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")    
+def clear_field():
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
+root = tk.Tk()
+root.geometry("300x300")
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
+shashank_1 = tk.Button(root, text="1", command=lambda:add_to_calculation(1), width=5, font=("Arial, 14"))
+shashank_1.grid(row=2, column=1)
+shashank_2 = tk.Button(root, text="2", command=lambda:add_to_calculation(2), width=5, font=("Arial, 14"))
+shashank_2.grid(row=2, column=2)
+shashank_3 = tk.Button(root, text="3", command=lambda:add_to_calculation(3), width=5, font=("Arial, 14"))
+shashank_3.grid(row=2, column=3)
+shashank_4 = tk.Button(root, text="4", command=lambda:add_to_calculation(4), width=5, font=("Arial, 14"))
+shashank_4.grid(row=3, column=1)
+shashank_5 = tk.Button(root, text="5", command=lambda:add_to_calculation(5), width=5, font=("Arial, 14"))
+shashank_5.grid(row=3, column=2)
+shashank_6 = tk.Button(root, text="6", command=lambda:add_to_calculation(6), width=5, font=("Arial, 14"))
+shashank_6.grid(row=3, column=3)
+shashank_7 = tk.Button(root, text="7", command=lambda:add_to_calculation(7), width=5, font=("Arial, 14"))
+shashank_7.grid(row=4, column=1)
+shashank_8 = tk.Button(root, text="8", command=lambda:add_to_calculation(8), width=5, font=("Arial, 14"))
+shashank_8.grid(row=4, column=2)
+shashank_9 = tk.Button(root, text="9", command=lambda:add_to_calculation(9), width=5, font=("Arial, 14"))
+shashank_9.grid(row=4, column=3)
+shashank_0 = tk.Button(root, text="0", command=lambda:add_to_calculation(0), width=5, font=("Arial, 14"))
+shashank_0.grid(row=5, column=2)
+shashank_add = tk.Button(root, text="+", command=lambda:add_to_calculation("+"), width=5, font=("Arial, 14"))
+shashank_add.grid(row=2, column=4)
+shashank_sub = tk.Button(root, text="-", command=lambda:add_to_calculation("-"), width=5, font=("Arial, 14"))
+shashank_sub.grid(row=3, column=4)
+shashank_mul = tk.Button(root, text="*", command=lambda:add_to_calculation("*"), width=5, font=("Arial, 14"))
+shashank_mul.grid(row=4, column=4)
+shashank_div = tk.Button(root, text="/", command=lambda:add_to_calculation("/"), width=5, font=("Arial, 14"))
+shashank_div.grid(row=5, column=4)
+shashank_open = tk.Button(root, text="(", command=lambda:add_to_calculation("("), width=5, font=("Arial, 14"))
+shashank_open.grid(row=5, column=1)
+shashank_close = tk.Button(root, text=")", command=lambda:add_to_calculation(")"), width=5, font=("Arial, 14"))
+shashank_close.grid(row=5, column=3)
+shashank_C = tk.Button(root, text="C", command=clear_field, width=11, font=("Arial, 14"))
+shashank_C.grid(row=6, column=1, columnspan=2)
+shashank_equal = tk.Button(root, text="=", command=evaluate_calculation, width=11, font=("Arial, 14"))
+shashank_equal.grid(row=6, column=3, columnspan=4)
+shashank_shashank = tk.Button(root, text="Designed By Shashank", command=evaluate_calculation, width=24, font=("Arial, 14"))
+shashank_shashank.grid(row=7, column=1, columnspan=4)
+root.mainloop()            
